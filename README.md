3956f2d6b4202e2d8c9539e58c761549c6984bdb
Alexander Sfakianos <alexander@sfakianos.com>
2019-11-04 20:20:36 -0500

    Base

---
+++ README.md
@@ -0,0 +1,3 @@
+# EECS 341 (Databases) Project
+
+Django-based web app with sqlalchemy
---
+++ db_proj/scraper/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+# Model for each item that we'd feature
+
+# Model for User if we need custom add-ons (we should bc + 1 table)
+
+# Model for sets of items/types.
+
+# Other bullshit models
