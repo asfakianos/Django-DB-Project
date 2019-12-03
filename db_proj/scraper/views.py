ddcfdd6750da853f8772bc0d17c8c7bbb6d22430
Alexander Sfakianos <alexander@sfakianos.com>
2019-12-03 01:01:08 -0500

    Can now view course pages @ course/COURSEID/, but no reviews yet

--- db_proj/scraper/views.py
+++ db_proj/scraper/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
@@ -135,18 +135,22 @@ class InstructorView(ListView):
 
 # Form for writing/submitting reviews
 # Render a form as well as class info.
-class CourseView(ListView, FormMixin):
-	template_name='scraper/base.html'
+class CourseView(ListView, SingleObjectMixin, FormMixin):
+	template_name='scraper/course_view.html'
 	form_class = CourseReviewForm
 
-	def get_queryset(self):
-		return Review.objects.all()
-
-	def get_context_data(self, *args, **kwargs):
-		context = super(CourseView, self).get_context_data(*args, **kwargs)
-		context['slug'] = self.kwargs['slug'] # Use this to find specific user
-		print(Course.objects.filter(course_id=context['slug']))
+	def get_context_data(self, **kwargs):
+		# context = super(CourseView, self).get_context_data(**kwargs)
+		 # Use this to find specific course and pass to context
+		context = {}
+		context['course'] = Course.objects.get(course_id=self.kwargs['slug'])
+		print(context)
 		return context
+
+
+	def get_queryset(self, **kwargs):
+		print("QUERY TIME")
+		return Review.objects.filter(course__course_id=Course.objects.get(course_id=self.kwargs['slug']).course_id)		
 
 
 class CustomView(ListView):
@@ -154,7 +158,6 @@ class CustomView(ListView):
 
 	def get_queryset(self):
 		query = self.request.GET
-		print(query['query']) # DEBUG
 		# Our query set should be received from raw sql!!!
 		sql_query = "SELECT course_id, units, name " + self._prepare_sql_query(query['query'])
 		print(sql_query)
